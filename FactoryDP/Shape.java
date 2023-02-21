47c341442c6c86e697e23f5c2d9078fe00141572
Fatmatrn <ftmtrn3488@gmail.com>
2023-02-21 11:57:26 +0300

    1

---
+++ FactoryDP/Shape.java
@@ -0,0 +1,13 @@
+package CreationalDP.FactoryDP;
+
+public interface Shape {
+
+    void draw();
+    /*
+    Factory Design Pattern
+    -SessionFactory ile bunu kullanmistik
+    -Kullanacagimiz bir obje nin uretilecegi sinifin ic yapisi her zaman bilmek zorunda olmayabiliriz
+    -Domain bilgisi olmaksizin bir nesneyi dogru bicimde uretmeyi saglayan yapidir.
+
+     */
+}
